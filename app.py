e235778829a2b6f78c24fe0eb391ebebd2791c45
Cassie Achzenick <caachzenick@gmail.com>
2020-05-28 17:06:14 -0600

    Add photo post endpoint
    Co-authored-by: jonathan-tschida <jonathan.tschida@outlook.com>

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import (
 )
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import requests
 
 app = Flask(__name__)
 
@@ -61,6 +62,24 @@ def update_plant(user_id, plant_id):
     mongo.db.plants.update_one({'_id': plant_id}, {'$set': request.json})
     updated_plant = mongo.db.plants.find_one({'_id': plant_id})
     return jsonify(updated_plant)
+
+@app.route('/api/v1/users/<int:user_id>/plants/<int:plant_id>/photos', methods=['POST'])
+def create_photo(user_id, plant_id):
+    user = mongo.db.users.find_one_or_404({'_id': user_id})
+    plant = mongo.db.plants.find_one_or_404({'_id': plant_id})
+    headers = {'Content-Type': 'application/pdf'}
+    files = {'file': request.files['file']}
+    r = requests.post('https://2ku6am910d.execute-api.us-west-1.amazonaws.com/v1/post-json/upload', headers=headers, files=files)
+    if r.status_code == 200:
+        photo = {
+            '_id': str(ObjectId()),
+            'plant_id': plant_id,
+            'photo_url': r.json()["body"]
+        }
+        mongo.db.photos.insert_one(photo)
+        return jsonify(photo)
+    return jsonify({"error": "Failed to upload"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
